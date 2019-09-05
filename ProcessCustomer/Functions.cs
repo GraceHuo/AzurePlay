6147585d2681faface5d4faa438dcdd92510a74b
Yan Huo <UA33443@DOW.COM>
2019-09-05 12:30:44 -0500

    Added a web job for processing

---
+++ ProcessCustomer/Functions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.WebJobs;
+using System.IO;
+
+namespace ProcessCustomer
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("customerqueue")] string message, TextWriter log)
+        {
+            // processing + email
+            log.WriteLine(message);
+        }
+    }
+}
